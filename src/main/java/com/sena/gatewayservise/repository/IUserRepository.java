e93559e11bace61123d1d02be4844180fa3855c6
Sena <darkphoenixq@gmail.com>
2022-04-21 14:57:17 +0300

    Initial commit

---
+++ src/main/java/com/sena/gatewayservise/repository/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.sena.gatewayservise.repository;
+
+import com.sena.gatewayservise.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface IUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/sena/gatewayservise/service/UserServiceImp.java
@@ -0,0 +1,39 @@
+package com.sena.gatewayservise.service;
+
+import com.sena.gatewayservise.model.User;
+import com.sena.gatewayservise.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImp implements IUserService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setCreateTime(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> findAllUsers()
+    {
+        return userRepository.findAll();
+    }
+}
